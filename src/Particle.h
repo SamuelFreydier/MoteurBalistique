56bdce71c27687c50b95763c0239c8f1459034b5
Solène <solene.tessiore@gmail.com>
2023-09-13 17:22:10 -0400

    Ajout de getter et setter pour les attributs

--- src/Particle.h
+++ src/Particle.h
@@ -13,6 +13,17 @@ private:
 public:
 	Particle(float mass, float velocity, float acceleration, Vector position);
 
+	const float GetMassReverse() const { return m_massReverse; }
+	void SetMassReverse(const float& mass) { m_massReverse = 1 / mass; }
+
+	const float GetVelocity() const { return m_velocity; }
+	void SetVelocity(const float& velocity) { m_acceleration = velocity; }
+
+	const float GetAcceleration() const { return m_acceleration; }
+	void SetAcceleration(const float& acceleration) { m_acceleration = acceleration; }
+
+	const Vector GetPosition() const { return m_position; }
+	void SetPosition(const Vector& position) { m_position = position; }
 };
 
 #endif
