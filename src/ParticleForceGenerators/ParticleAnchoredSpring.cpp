8fb5f6a043a395bc3d59b81027357f4aaf90c87d
Samuel Freydier <samuel.freydier@gmail.com>
2023-10-11 16:54:20 -0400

    Ajout du ressort avec extrémité fixe

---
+++ src/ParticleForceGenerators/ParticleAnchoredSpring.cpp
@@ -0,0 +1,38 @@
+#include "ParticleSpring.h"
+#include "ParticleAnchoredSpring.h"
+
+/**
+ * @brief Constructeur à partir d'un point fixe de l'espace, de la constante d'élasticité et de la longueur au repos du ressort
+ * @param anchor 
+ * @param springConstant 
+ * @param restLength 
+*/
+ParticleAnchoredSpring::ParticleAnchoredSpring( const std::shared_ptr<Vector3>& anchor, const float& springConstant, const float& restLength )
+    : m_anchor( anchor ), m_springConstant( springConstant ), m_restLength( restLength )
+{
+}
+
+
+/**
+ * @brief Applique la force de ressort à particle. Formule : f = -k (l - l0)
+ * @param particle
+ * @param duration
+*/
+void ParticleAnchoredSpring::updateForce( std::shared_ptr<Particle> particle, float duration )
+{
+    // Vecteur du ressort
+    Vector3 force( particle->getPosition() );
+    force -= *m_anchor;
+
+    // Norme de la force
+    float norm = force.norm(); // norm = l
+    norm = fabs( norm - m_restLength ); // norm = l - l0
+    norm *= m_springConstant; // norm = k * (l - l0)
+
+    // Calcul de la force finale
+    force.normalize(); // Direction du vecteur force
+    force *= -norm; // force = - k * (l - l0) dans la bonne direction
+
+    // Ajout de la force
+    particle->addForce( force );
+}
---
+++ src/ParticleForceGenerators/ParticleAnchoredSpring.h
@@ -0,0 +1,25 @@
+#ifndef PARTICLEANCHOREDSPRING_H
+#define PARTICLEANCHOREDSPRING_H
+
+#include "ParticleForceGenerator.h"
+
+// Applique une force de ressort, avec une extrémité fixée à un point de l'espace
+class ParticleAnchoredSpring : public ParticleForceGenerator
+{
+private:
+    // Ancre
+    std::shared_ptr<Vector3> m_anchor;
+
+    // Constante d'élasticité
+    float m_springConstant;
+
+    // Longueur du ressort au repos
+    float m_restLength;
+
+public:
+    ParticleAnchoredSpring( const std::shared_ptr<Vector3>& anchor, const float& springConstant, const float& restLength );
+
+    virtual void updateForce( std::shared_ptr<Particle> particle, float duration ) override;
+};
+
+#endif
